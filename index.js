3252135a2510de26b90e90b2c11f6dbcdb7b85c3
Jim Caldwell <jim.caldwell.99@gmail.com>
2020-01-16 16:39:49 -0600

    More updates

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
 const MailParser  = require('mailparser').MailParser;
+const assert     = require('assert');
+//const { expect } = require('chai');
+//const through2   = require('through2');
+const simpleParser = require('mailparser').simpleParser;
+const mysql      = require('mysql');
+
+program.version('1.0.0');
+program.option("-p, --path <path>", "Input path (file or directory)", "./mbox")
+       .option("-c, --config <config>", "Config file", "./config/config.json")
+       .usage("[global options]")
+       .parse(process.argv);
+
+console.log("Welcome to Yahoo Group Archive to WebView");
+console.log("  Input path:" + program.path);
+console.log("  Config file:" + program.config);
+
+// load config file
+try {
+    var stat = fs.statSync(program.config)
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+} finally {
+}
+let rawdata = fs.readFileSync(program.config);
+let config = JSON.parse(rawdata);
+
+// add single file or all files in directory in filesCollection array
+let isPathDirectory = true;
+try {
+    isPathDirectory = fs.statSync(program.path).isDirectory();
+} catch (e) {
+  console.error(e);
+} finally {
+}
+
+var filesCollection = [];
+if (isPathDirectory) {
+   readDirectorySynchronously(program.path);
+} else {
+   filesCollection.push(program.path); // add single file
+}
 
 
+function readDirectorySynchronously(directory) {
+    var currentDirectory = fs.readdirSync(directory, 'utf8');
+
+    currentDirectory.forEach(file => {
+//        var fileShouldBeSkipped = directoriesToSkip.indexOf(file) > -1;
+        var pathOfCurrentItem = path.join(directory + '/' + file);
+        if (/*!fileShouldBeSkipped && */ fs.statSync(pathOfCurrentItem).isFile()) {
+            filesCollection.push(pathOfCurrentItem);
+        }
+        else {  //} if (!fileShouldBeSkipped) {
+            var directorypath = path.join(directory + '/' + file);
+            readDirectorySynchronously(directorypath);
+        }
+    });
+}
+
+console.log(config);
+
+console.log("Connect to db...");
+// open database connection
+var connection = mysql.createConnection({
+  host     : config.db_host,
+  user     : config.db_user,
+  password : config.db_password,
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting to database: ' + err.stack);
+    process.exit(1);
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
 
-var myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
-
-
+// now time to parse each input file into
 /*
 
 const Mbox       = require('./src/mbox');
-const assert     = require('assert');
-const { expect } = require('chai');
-const fs         = require('fs');
-const through2   = require('through2');
-const simpleParser = require('mailparser').simpleParser;
 
 const mbox = new Mbox('50795.mbox.00001', {  });
 
@@ -80,3 +150,6 @@ function dispHeaders(value, key, map) {
 }
 
 */
+
+
+connection.end();
